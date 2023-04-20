541a768c23bd79a63988fa02bf8df722f2fef211
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-04-20 12:06:46 -0300

    WIP: drag and drop implementation

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -2,7 +2,7 @@ import ProductsList from "../../components/ProductsList";
 
 export default function Main() {
   return ( 
-    <div>
+    <div className="w-full">
       LISTA DE COMPRAS
       <ProductsList products={['Leite', 'Batata Palha', 'Manteiga', 'Queijo', 'Iogurte']}/>
     </div>
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,20 +1,43 @@
-'use client'
-import { Product } from "@/models/Product.model";
-import { List, ListIcon, ListItem } from "@chakra-ui/react";
-import { IconCircle } from "@tabler/icons-react";
+'use client';
+import { Product } from '@/models/Product.model';
+import { List, ListIcon, ListItem } from '@chakra-ui/react';
+import { IconCircle, IconGripVertical } from '@tabler/icons-react';
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
+import { useCallback } from 'react';
 
-function ProductsList({ products }:{ products: Product[] }) {
-  return (
-    <List spacing={10}>
-      {
-        products.length > 0 && products.map((product: Product) => (
-          <ListItem key={product}>
-            <ListIcon as={IconCircle}/>
-            {product}
+function ProductsList({ products }: { products: Product[] }) {
+  const onDragEnd = useCallback(() => {}, []);
+
+  function getDraggableList(products: Product[]) {
+    return products.map((product: Product, i) => (
+      <Draggable key={product} draggableId={product} index={i}>
+        {(provided, snapshot) => (
+          <ListItem
+            className="bg-foreground-base py-2 flex items-center rounded-md text-text-main-base"
+          >
+            <ListIcon
+              as={IconCircle}
+              className="mx-2 text-text-secondary-base"
+            />
+            <span className="flex-grow">{product}</span>
+            <IconGripVertical
+              className="text-text-secondary-base mx-2"
+              size={20}
+            />
           </ListItem>
-        ))
-      }
-    </List>
+        )}
+      </Draggable>
+    ))
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <List spacing={12}>
+        <Droppable droppableId='mainList'>
+          {getDraggableList(products)}
+        </Droppable>
+      </List>
+    </DragDropContext>
   );
 }
 
