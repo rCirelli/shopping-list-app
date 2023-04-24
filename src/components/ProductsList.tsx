4eb97e7e7c1b19e1c03ee52db02e51e7f4dbbe1c
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-04-24 12:01:03 -0300

    feat: list drag and drop implementation

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -2,11 +2,33 @@
 import { Product } from '@/models/Product.model';
 import { List, ListIcon, ListItem } from '@chakra-ui/react';
 import { IconCircle, IconGripVertical } from '@tabler/icons-react';
-import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
-import { useCallback } from 'react';
+import { DragDropContext, Droppable, Draggable, DropResult } from '@hello-pangea/dnd';
+import { useCallback, useState } from 'react';
 
 function ProductsList({ products }: { products: Product[] }) {
-  const onDragEnd = useCallback(() => {}, []);
+  const [ productsList, setProductsList ] = useState(products);
+
+  const reorderList = useCallback((startIndex: number, endIndex: number) => {
+    const result = [...productsList];
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  }, [productsList]);
+
+  const onDragEnd = useCallback((result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorderList(
+      result.source.index,
+      result.destination.index
+    );
+
+    setProductsList(items);
+  }, [reorderList]);
+
 
   function getDraggableList(products: Product[]) {
     return products.map((product: Product, i) => (
@@ -14,29 +36,40 @@ function ProductsList({ products }: { products: Product[] }) {
         {(provided, snapshot) => (
           <ListItem
             className="bg-foreground-base py-2 flex items-center rounded-md text-text-main-base"
+            ref={provided.innerRef}
+            {...provided.draggableProps}
           >
             <ListIcon
               as={IconCircle}
               className="mx-2 text-text-secondary-base"
             />
             <span className="flex-grow">{product}</span>
-            <IconGripVertical
-              className="text-text-secondary-base mx-2"
-              size={20}
-            />
+            <div {...provided.dragHandleProps}>
+              <IconGripVertical
+                className="text-text-secondary-base mx-2"
+                size={20}
+              />
+            </div>
           </ListItem>
         )}
       </Draggable>
-    ))
+    ));
   }
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <List spacing={12}>
-        <Droppable droppableId='mainList'>
-          {getDraggableList(products)}
-        </Droppable>
-      </List>
+      <Droppable droppableId="mainList">
+        {(provided, snapshot) => (
+          <List
+            spacing={12}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {getDraggableList(productsList)}
+            {provided.placeholder}
+          </List>
+        )}
+      </Droppable>
     </DragDropContext>
   );
 }
